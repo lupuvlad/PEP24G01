9dc30299d6008fecd53dc803aae2e13920727982
lupuvlad <lupuvlad59@gmail.com>
2024-01-29 21:01:54 +0200

    Average, integer division and power

---
+++ modul2/ex4.py
@@ -0,0 +1,15 @@
+# Get 2 numbers from the user
+num1 = int(input("num1 = "))
+num2 = int(input("num2 = "))
+
+# Calculate the average of the num1 and num2
+average = (num1 + num2) / 2
+print("Average =", average)
+
+# Integer division
+int_division = num1 // num2
+print("Integer division =", int_division)
+
+# num1 ** num2
+power = num1 ** num2
+print("num1 ** num2 =", power)
