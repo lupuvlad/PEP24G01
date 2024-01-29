50a661506f652ff9695d3c1cc59a8bdb7ac48896
lupuvlad <lupuvlad59@gmail.com>
2024-01-29 20:37:50 +0200

    Arithmetic operations

---
+++ modul2/part1_2.py
@@ -0,0 +1,28 @@
+int1 = 100
+int2 = 200
+
+result = int1 + int2
+print(result)
+
+new_result = result - int1
+print(new_result)
+
+# Square root
+sqrt = int1 ** (1/2)
+print(sqrt)
+
+# Float
+add1 = 10.1 + 10.2
+print(add1)
+
+# Set a method directly to an int number
+add_result = (10).__add__(10)
+print(add_result)
+
+# Modulo
+modulo_result = 10 % 3
+print(modulo_result)
+
+# Integer division
+result = 10 // 3
+print(result)
