b58a485e4ac60dda59572e8a02c941fa600994b9
lupuvlad <lupuvlad59@gmail.com>
2024-01-31 20:37:10 +0200

    Print practice

---
+++ modul2/ex6.py
@@ -0,0 +1,9 @@
+print('Astazi ma duc la "facultate".')
+
+print(r"""/*\/*\*/*\/*\
+Python
+\./\./\./\./""")
+
+print("P", "y", "t", "h", "o", "n", sep="   ")
+# Print with tab
+print("P\ty\tt\th\to\tn")
