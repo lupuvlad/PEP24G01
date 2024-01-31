21acc57e1d2f46ed0134575939a8fcac466558f2
lupuvlad <lupuvlad59@gmail.com>
2024-01-31 21:12:00 +0200

    Print different formats of strings

---
+++ modul2/ex11.py
@@ -0,0 +1,19 @@
+# Print the strings
+
+# Separated by _
+print("Hello", "Python", sep="_")
+print("Ana", "are", "mere", sep="_")
+print("Pizza", "Party", sep="_")
+
+# With . at the end of each string
+print("Hello", "Python", end=".")
+print("Ana", "are", "mere", end=".")
+print("Pizza", "Party", end=".")
+
+# Blank line
+print("")
+
+# First word multiplied by 4
+print("Hello" * 4, "Python")
+print("Ana" * 4, "are", "mere")
+print("Pizza" * 4, "Party")
