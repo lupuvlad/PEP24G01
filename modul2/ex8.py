b6320f6b9c244b26b807320c9bc7fea029ea04eb
lupuvlad <lupuvlad59@gmail.com>
2024-01-31 20:41:27 +0200

    Length of a string practice

---
+++ modul2/ex8.py
@@ -0,0 +1,7 @@
+# Get a string from user input
+user_string = input("Enter a string: ")
+
+print("Lungimea sirului este: {}".format(len(user_string)))
+print(f"Lungimea sirului este: {len(user_string)}")
+print("Lungimea sirului este: " + str(len(user_string)))
+print("Lungimea sirului este:", str(len(user_string)))
