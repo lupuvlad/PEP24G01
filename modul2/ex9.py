e6447c1f8538d09559b702fcd7e3b4e6c5e22dac
lupuvlad <lupuvlad59@gmail.com>
2024-01-31 21:11:21 +0200

    Print geometric forms practice

---
+++ modul2/ex9.py
@@ -0,0 +1,28 @@
+width = 30
+
+# First geometric form
+print("a.")
+print(r"/-\ ".center(width))
+print(r"//_\\".center(width))
+print("-------".center(width))
+print(r"\\_//".center(width))
+print(r"\-/".center(width))
+
+# Print blank line
+print("")
+
+# Second geometric form
+print("b.")
+print("----".center(width))
+print("/    \\".center(width))
+print("/______\\".center(width))
+
+# Print blank line
+print("")
+
+# Print the third geometric form
+print("c.")
+print("*".center(width))
+print(("*" * 3).center(width))
+print(("*" * 5).center(width))
+print(("*" * 7).center(width))
