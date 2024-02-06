deada15c24773a8da740d752bd662c1bc3e72dbd
lupuvlad <lupuvlad59@gmail.com>
2024-02-06 23:34:00 +0200

    Improved the code

--- modul3/tema1.py
+++ modul3/tema1.py
@@ -1,27 +1,24 @@
 import time
 
-options = [["1", 4], ["2", 3.5]]
+options = [["cappuccino", 4], ["espresso", 3.5]]
 
-print(f"""1. Cappuccino ... 4 lei
-2. Espresso ... 3.5 lei""")
-user_option = input("Choose an option (1 or 2): ")
+for i in range(len(options)):
+    print(f"{i+1}. {options[i][0].title()}... {options[i][1]} lei")
+
+coffee = input(f"Choose an option ({options[0][0]}/{options[1][0]}): ")
 
 for option in options:
-    if user_option == option[0]:
-        total = int(input("Insert only 5 or 10 lei: "))
-        if total == 5 or total == 10:
-            change = total - option[1]
+    if coffee == option[0]:
+        pay = int(input("Insert only 5 or 10 lei: "))
+        if pay == 5 or pay == 10:
+            change = pay - option[1]
             print(f"You will receive {change} lei change.")
-            print("Preparing your coffee...")
+            print(f"Preparing your {coffee}...")
             time.sleep(3)
-            print("Enjoy your coffee!")
+            print(f"Enjoy your {coffee}!")
             break
         else:
             print("The coffee machine accepts only bank-notes of 5 or 10 lei.")
             break
 else:
     print("This option doesn't exist")
-
-
-
-
