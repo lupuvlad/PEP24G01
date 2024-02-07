d90f29bbc31ca55138d67267c2cca0bca6f1d3ea
lupuvlad <lupuvlad59@gmail.com>
2024-02-07 20:34:46 +0200

    Delete line with print statement

--- modul3/tema2.py
+++ modul3/tema2.py
@@ -4,8 +4,7 @@ guess = int(input("Enter the password: "))
 
 while guess != PC_pass:
     if len(str(guess)) == 4:
-        print("Wrong password! Try again.")
-        guess = int(input("Enter the password: "))
+        guess = int(input("Wrong password! Try again: "))
         if guess == PC_pass:
             print("Access granted")
             break
