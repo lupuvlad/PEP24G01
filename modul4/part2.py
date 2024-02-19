2ea12af40d6e2052dc4d616b86065efc776be42f
lupuvlad <lupuvlad59@gmail.com>
2024-02-19 23:00:41 +0200

    Dictionaries

---
+++ modul4/part2.py
@@ -0,0 +1,20 @@
+# Dictionary
+
+# my_dict = {'one': 1, 'two': 2, 'true': True}
+#
+# print(my_dict)
+# print(type(my_dict))
+#
+# my_dict = dict(one=1, two=2)
+# print(my_dict)
+#
+# # iterate over dictionary
+# for elem in my_dict:  # same as my_dict.keys()
+#     print("dict key:", elem)
+#
+# for elem in my_dict.values():
+#     print("dict values:", elem)
+#
+# for elem in my_dict.items():
+#     print("dict items:", elem)
+
