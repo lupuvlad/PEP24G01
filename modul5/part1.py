1521f49b2539b411a140aebeb5b8bb2f99960fea
lupuvlad <lupuvlad59@gmail.com>
2024-02-21 20:55:20 +0200

    Modules and packages

---
+++ modul5/part1.py
@@ -0,0 +1,25 @@
+# import math as m
+# from math import log
+#
+#
+# # def m():
+# #     pass
+#
+# print(m.log(100, 10))
+# print(log(100, 10))
+#
+# from math import *
+# print(ceil(23.3))
+# print(floor(23.3))
+#
+# # use import
+# print(f"Script name is {__name__}")
+# from modul4.app1 import check_passwd
+#
+# check_passwd()
+
+
+from modul5 import my_package
+
+print(my_package.var1)
+my_package.test_function()
